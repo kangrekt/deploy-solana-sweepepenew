25cd8b93e12dd23ca3db4b1e8b60778b8fd03dad
kang rekt <58033250+kangrekt@users.noreply.github.com>
2026-08-01 05:58:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         document.title = "$SWEEP - Stop Wasting Everything";
     } else if (chartIframe) {
-        chartIframe.src = `https://dexscreener.com/solana/${TOKEN_CA}?embed=1&theme=dark&trades=0&info=0`;
+        chartIframe.src = `https://dexscreener.com/solana/${TOKEN_CA}?embed=1&theme=dark`;
     }
 
     // Update Solscan Holders link dynamically
